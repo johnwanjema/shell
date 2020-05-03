58b50b11feb3a39f7d6f3586307a181fed2ce08e
john <jonwanjema@gmail.com>
2020-05-03 10:13:36 +0300

    added an array of names

--- hello.sh
+++ hello.sh
@@ -4,3 +4,15 @@
 echo "What is your name?"
 read PERSON
 echo "Hello, $PERSON"
+
+
+NAME[0]="Zara"
+NAME[1]="Qadir"
+NAME[2]="Mahnaz"
+NAME[3]="Ayan"
+NAME[4]="Daisy"
+## NAME: readonly variable
+echo "First Index: ${NAME[0]}"
+echo "Second Index: ${NAME[1]}"
+
+
