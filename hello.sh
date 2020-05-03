12e94f8586a1045a4e7a97024d6a38260e5b3413
john <jonwanjema@gmail.com>
2020-05-03 10:15:09 +0300

    added methods to acess all values in an array at once

--- hello.sh
+++ hello.sh
@@ -11,8 +11,11 @@ NAME[1]="Qadir"
 NAME[2]="Mahnaz"
 NAME[3]="Ayan"
 NAME[4]="Daisy"
-## NAME: readonly variable
-echo "First Index: ${NAME[0]}"
-echo "Second Index: ${NAME[1]}"
+## acess array values one by one
+# echo "First Index: ${NAME[0]}"
+# echo "Second Index: ${NAME[1]}"
 
+#access all values in an array
+echo "First Method: ${NAME[*]}"
+echo "Second Method: ${NAME[@]}"
 
